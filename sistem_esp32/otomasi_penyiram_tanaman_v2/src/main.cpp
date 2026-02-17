9803b3bfa08d4c779c4486add70b1cd32d7350e1
Jati Prakoso <jatip0311@gmail.com>
2026-02-18 00:01:50 +0700

    interaksi pengguna dengan display dan penambahan library
    perlu di cek ulang

--- sistem_esp32/otomasi_penyiram_tanaman_v2/src/main.cpp
+++ sistem_esp32/otomasi_penyiram_tanaman_v2/src/main.cpp
@@ -5,7 +5,8 @@
 #include <LiquidCrystal_I2C.h>
 #include <TM1637Display.h>
 #include <RTClib.h>
-
+#include <Encoder.h>
+#include <Preferences.h>
 
 //konfigurasi wifi
 const char* ssid     = "MBAK ARTHA 4G";
@@ -52,6 +53,7 @@ void setup() {
   lcd.init();
   lcd.backlight();
   tm.setBrightness(0x0f);
+  tm.clear();
   //koneksi ke wifi
   lcd.setCursor(0, 0);
   lcd.print("Menyambungkan...");
@@ -65,6 +67,12 @@ void setup() {
   Serial.println("WiFi tersambung!");
   //sinkronisasi waktu dengan ntp
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  //tombol input
+  pinMode(button_pin_set_waktu, INPUT_PULLUP);
+  pinMode(button_pin_onoff_otomatis, INPUT_PULLUP);
+  pinMode(button_pin_siram_manual, INPUT_PULLUP);
+  //rotary encoder
+  
 }
 
 void loop() {
@@ -78,7 +86,153 @@ void loop() {
   //tampilkan waktu di serial monitor dan lcd
   char timeStr[9];
   
+  //cek kodingan besok ---
+  // Persistent prefs (loaded once)
+  static Preferences prefs;
+  static bool prefsLoaded = false;
+  static bool autoEnabled = true;
+  static bool wateringActive = false;
+  static unsigned long wateringEndMillis = 0;
+  static int lastCheckedMinute = -1;
+  static int lastWateredMinute = -1;
+  if (!prefsLoaded) {
+    prefs.begin("siram", false);
+    jamSiram_1 = prefs.getInt("j1h", jamSiram_1);
+    menitSiram_1 = prefs.getInt("j1m", menitSiram_1);
+    jamSiram_2 = prefs.getInt("j2h", jamSiram_2);
+    menitSiram_2 = prefs.getInt("j2m", menitSiram_2);
+    durasiSiram = prefs.getInt("dur", durasiSiram);
+    autoEnabled = prefs.getBool("auto", true);
+    prefsLoaded = true;
+  }
+
+  // Button / setting handling
+  enum SetMode {MODE_NONE=0, MODE_H1, MODE_M1, MODE_H2, MODE_M2, MODE_DUR};
+  static SetMode setMode = MODE_NONE;
+  static unsigned long lastButtonTime = 0;
+  const unsigned long debounce = 50;
+  static int lastSetButtonState = HIGH;
+  static int lastIncButtonState = HIGH;
+  static int lastManualButtonState = HIGH;
+
+  int setBtn = digitalRead(button_pin_set_waktu);
+  int incBtn = digitalRead(button_pin_onoff_otomatis);
+  int manualBtn = digitalRead(button_pin_siram_manual);
+  unsigned long nowMillis = millis();
+
+  // set button: cycle through fields
+  if (setBtn != lastSetButtonState && nowMillis - lastButtonTime > debounce) {
+    lastButtonTime = nowMillis;
+    if (setBtn == LOW) {
+      if (setMode == MODE_NONE) setMode = MODE_H1;
+      else if (setMode == MODE_H1) setMode = MODE_M1;
+      else if (setMode == MODE_M1) setMode = MODE_H2;
+      else if (setMode == MODE_H2) setMode = MODE_M2;
+      else if (setMode == MODE_M2) setMode = MODE_DUR;
+      else {
+        setMode = MODE_NONE;
+        // save settings
+        prefs.putInt("j1h", jamSiram_1);
+        prefs.putInt("j1m", menitSiram_1);
+        prefs.putInt("j2h", jamSiram_2);
+        prefs.putInt("j2m", menitSiram_2);
+        prefs.putInt("dur", durasiSiram);
+      }
+    }
+  }
+  lastSetButtonState = setBtn;
+
+  // inc button: increment field or toggle auto
+  if (incBtn != lastIncButtonState && nowMillis - lastButtonTime > debounce) {
+    lastButtonTime = nowMillis;
+    if (incBtn == LOW) {
+      if (setMode == MODE_NONE) {
+        autoEnabled = !autoEnabled;
+        prefs.putBool("auto", autoEnabled);
+      } else {
+        if (setMode == MODE_H1) jamSiram_1 = (jamSiram_1 + 1) % 24;
+        else if (setMode == MODE_M1) menitSiram_1 = (menitSiram_1 + 1) % 60;
+        else if (setMode == MODE_H2) jamSiram_2 = (jamSiram_2 + 1) % 24;
+        else if (setMode == MODE_M2) menitSiram_2 = (menitSiram_2 + 1) % 60;
+        else if (setMode == MODE_DUR) durasiSiram = max(1, min(120, durasiSiram + 1));
+      }
+    }
+  }
+  lastIncButtonState = incBtn;
+
+  // manual watering button: start watering immediately
+  if (manualBtn != lastManualButtonState && nowMillis - lastButtonTime > debounce) {
+    lastButtonTime = nowMillis;
+    if (manualBtn == LOW) {
+      wateringActive = true;
+      wateringEndMillis = nowMillis + (unsigned long)durasiSiram * 60000UL;
+      digitalWrite(relay_pin_1, LOW);
+      digitalWrite(relay_pin_2, LOW);
+    }
+  }
+  lastManualButtonState = manualBtn;
+
+  // update watering state non-blocking
+  if (wateringActive && nowMillis >= wateringEndMillis) {
+    wateringActive = false;
+    digitalWrite(relay_pin_1, HIGH);
+    digitalWrite(relay_pin_2, HIGH);
+  }
+
+  // automatic watering on minute tick
+  int curHour = timeinfo.tm_hour;
+  int curMin = timeinfo.tm_min;
+  if (curMin != lastCheckedMinute) {
+    lastCheckedMinute = curMin;
+    if (autoEnabled && !wateringActive) {
+      if ((curHour == jamSiram_1 && curMin == menitSiram_1) || (curHour == jamSiram_2 && curMin == menitSiram_2)) {
+        int key = curHour*100 + curMin;
+        if (lastWateredMinute != key) {
+          lastWateredMinute = key;
+          wateringActive = true;
+          wateringEndMillis = nowMillis + (unsigned long)durasiSiram * 60000UL;
+          digitalWrite(relay_pin_1, LOW);
+          digitalWrite(relay_pin_2, LOW);
+        }
+      }
+    }
+  }
+
+  // display: show time or editing values
+  static unsigned long lastBlink = 0;
+  static bool blinkOn = true;
+  if (nowMillis - lastBlink >= 500) { lastBlink = nowMillis; blinkOn = !blinkOn; }
+
+  if (setMode == MODE_DUR) {
+    // show duration as right two digits
+    int minutes = durasiSiram % 100;
+    int num = minutes;
+    tm.showNumberDecEx(num, 0, true, 4, 0);
+  } else {
+    int showH = curHour;
+    int showM = curMin;
+    if (setMode == MODE_H1) showH = (blinkOn ? jamSiram_1 : curHour);
+    else if (setMode == MODE_M1) showM = (blinkOn ? menitSiram_1 : curMin);
+    else if (setMode == MODE_H2) showH = (blinkOn ? jamSiram_2 : curHour);
+    else if (setMode == MODE_M2) showM = (blinkOn ? menitSiram_2 : curMin);
+    int number = showH * 100 + showM;
+    uint8_t colon = (blinkOn ? 0x40 : 0x00);
+    tm.showNumberDecEx(number, colon, true, 4, 0);
+  }
+
+  // small lcd status
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Waktu:");
+  sprintf(timeStr, "%02d:%02d", curHour, curMin);
+  lcd.setCursor(6,0);
+  lcd.print(timeStr);
+  lcd.setCursor(0,1);
+  if (autoEnabled) lcd.print("Auto:ON "); else lcd.print("Auto:OFF");
+  lcd.setCursor(9,1);
+  lcd.print("D:"); lcd.print(durasiSiram);
 }
+//
 
 // // put function definitions here:
 // int myFunction(int x, int y) {
